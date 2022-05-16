2577602124b09c90258381064c669b6bd8a98812
linebot0705 <jim880705@gmail.com>
2022-05-16 15:56:04 +0800

    first commit

---
+++ 1005_hello.c
@@ -0,0 +1,14 @@
+/*
+說明
+請撰寫一個可以獨立執行的程式，可以是hello.c/hello.cpp/hello.py類型的檔案
+使其可以在螢幕上列印出下列指定文字
+*/
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World!");
+    printf("This is my first program");
+
+    return 0;
+}
