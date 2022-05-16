c9bf3e4910d588a17afaafcbcf893b536405711a
linebot0705 <jim880705@gmail.com>
2022-05-16 16:24:32 +0800

    ok

--- 1005_hello/1005_hello.c
+++ 1005_hello/1005_hello.c
@@ -1,5 +1,4 @@
 /*
-說明
 請撰寫一個可以獨立執行的程式，可以是hello.c/hello.cpp/hello.py類型的檔案
 使其可以在螢幕上列印出下列指定文字
 */
@@ -7,8 +6,8 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello World!");
-    printf("This is my first program");
+    printf("Hello World!\n");
+    printf("This is my first program.\n");
 
     return 0;
 }
