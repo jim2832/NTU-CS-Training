44b211c6540334a439e51194dbd742e6a9a473f4
Jim Lee <jim880705@gmail.com>
2022-09-27 22:38:43 +0800

    220927

---
+++ LV10/1112_square1/square.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "square.h"
+using namespace std;
+
+int Square::area(){
+    int area = len * len;
+    return area;
+}
---
+++ LV10/1112_square1/square.h
@@ -0,0 +1,10 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+class Square{
+    public:
+        int len;
+        int area();
+};
+
+#endif
